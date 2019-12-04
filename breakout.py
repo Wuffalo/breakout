50a654f59c44f42f7c9d5c149342aa63869fda5f
Wuffalo <waminskey@gmail.com>
2019-12-03 22:05:36 -0600

    Fix formatting to be dataframe specific (variable lengths). Removed test code. Fixed for empty df use cases.

--- breakout.py
+++ breakout.py
@@ -16,9 +16,16 @@ from datetime import datetime as dt, timedelta
 if os.path.exists("/mnt/c/Users/WMINSKEY/.pen/Breakout_py.xlsx"):
   os.remove("/mnt/c/Users/WMINSKEY/.pen/Breakout_py.xlsx")
 
-test_code = False
+path_to_output = "/mnt/c/Users/WMINSKEY/.pen/Breakout_py.xlsx"
 
-def format_sheet():
+show_DSLC = True
+show_ROANOKE = True
+show_RLCA = True
+show_WWT = True
+show_IngramMX = True
+
+def format_sheet(X):
+    X = X+1
     worksheet.set_column('A:A',13)
     worksheet.set_column('B:B',45)
     worksheet.set_column('C:C',5)
@@ -29,30 +36,20 @@ def format_sheet():
     worksheet.set_column('H:H',4)
     worksheet.set_column('I:I',27)
     worksheet.set_column('J:J',13,format5)
-    worksheet.conditional_format('J2:J'+str(len(df.index)+1), {'type': 'duplicate',
+    worksheet.conditional_format('J2:J'+str(X), {'type': 'duplicate',
                                         'format': format3})
-    worksheet.conditional_format('E2:E'+str(len(df.index)+1), {
+    worksheet.conditional_format('E2:E'+str(X), {
         'type': 'date',
         'criteria': 'less than',
         'value': (dt.now()-timedelta(1)),
         'format': format1
         })
-    worksheet.autofilter('A1:J'+str(len(df.index)+1))
+    worksheet.autofilter('A1:J'+str(X))
 
-if test_code == True:
-    path_to_SOS = "/mnt/c/Users/WMINSKEY/.pen/SOS.csv" # change to latest_file
-else:
-    list_of_files = glob.glob('/mnt/c/Users/WMINSKEY/Downloads/Shipment Order Summary -*.csv') # * means all if need specific format then *.csv
-    latest_file = max(list_of_files, key=os.path.getctime)
-    path_to_SOS = latest_file
 
-path_to_excel = "/mnt/c/Users/WMINSKEY/.pen/Breakout_py.xlsx"
-
-show_DSLC = True
-show_ROANOKE = True
-show_RLCA = True
-show_WWT = True
-show_IngramMX = True
+list_of_files = glob.glob('/mnt/c/Users/WMINSKEY/Downloads/Shipment Order Summary -*.csv') # * means all if need specific format then *.csv
+latest_file = max(list_of_files, key=os.path.getctime)
+path_to_SOS = latest_file
 
 df = pd.read_csv(path_to_SOS, parse_dates=[11,19], infer_datetime_format=True)
 
@@ -66,7 +63,7 @@ df = df.rename(columns={'EXTERNORDERKEY':'SO-SS','C_COMPANY':'Customer','ADDDATE
                         'TOTALORDERED':'QTY','SVCLVL':'Carrier','EXTERNALLOADID':'Load ID','EDITDATE':'Last Edit',
                         'C_STATE':'State','C_COUNTRY':'Country','Textbox6':'TIS'})
 
-writer = pd.ExcelWriter(path_to_excel, engine='xlsxwriter')
+writer = pd.ExcelWriter(path_to_output, engine='xlsxwriter')
 workbook = writer.book
 
 # Light red fill with dark red text.
@@ -96,59 +93,57 @@ df.sort_values(by=['Status','Carrier','Customer','Last Edit','Load ID'], inplace
 #drop columns - SECOND PASS
 df = df.drop(columns=['TYPEDESCR','CUSTID','PROMISEDATE','Last Edit'])
 
-#Check if dataframes are empty
-if DSLC.empty == True:
-    show_DSLC = False
-if ROANOKE.empty == True:
-    show_ROANOKE = False
-if RLCA.empty == True:
-    show_RLCA = False
-if WWT.empty == True:
-    show_WWT = False
-if IngramMX.empty == True:
-    show_IngramMX = False
+#find lengths of main dataframe and each query
+main_length = len(df.index)
+DSLC_length = df[DSLC].TIS.count()
+Roanoke_length = df[ROANOKE].TIS.count()
+RLCA_length = df[RLCA].TIS.count()
+WWT_length = df[WWT].TIS.count()
+IngramMX_length = df[IngramMX].TIS.count()
 
-#Give preview of queries
-if test_code == True:
-    print("DSLC Orders: \n",df[DSLC].head(2))
-    print("Roanoke Orders: \n",df[ROANOKE].head(2))
-    print("RLCA Orders: \n",df[RLCA].head(2))
-    print("WWT Orders: \n",df[WWT].head(2))
-    print("Ingram MX Orders: \n",df[IngramMX].head(2))
+#Check if dataframes are empty
+if DSLC_length == 0:
+    show_DSLC = False
+if Roanoke_length == 0:
+    show_ROANOKE = False
+if RLCA_length == 0:
+    show_RLCA = False
+if WWT_length == 0:
+    show_WWT = False
+if IngramMX_length == 0:
+    show_IngramMX = False
 
 #create and format main sheet of all orders
 df.to_excel(writer, sheet_name='Main', index=False)
 worksheet = writer.sheets['Main']
-format_sheet()
+format_sheet(main_length)
+writer.sheets['Main'].set_tab_color('yellow')
 
 #create various sheets if group type is present
 if show_DSLC == True:
     df[DSLC].to_excel(writer, sheet_name='DSLC', index=False)
     worksheet = writer.sheets['DSLC']
-    format_sheet()
+    format_sheet(DSLC_length)
+    writer.sheets['DSLC'].set_tab_color('green')
 if show_ROANOKE == True:
     df[ROANOKE].to_excel(writer, sheet_name='Roanoke', index=False)
     worksheet = writer.sheets['Roanoke']
-    format_sheet()
+    format_sheet(Roanoke_length)
+    writer.sheets['Roanoke'].set_tab_color('orange')
 if show_RLCA == True:
     df[RLCA].to_excel(writer, sheet_name='RLCA', index=False)
     worksheet = writer.sheets['RLCA']
-    format_sheet()
+    format_sheet(RLCA_length)
+    writer.sheets['RLCA'].set_tab_color('red')
 if show_WWT == True:
     df[WWT].to_excel(writer, sheet_name='WWT', index=False)
     worksheet = writer.sheets['WWT']
-    format_sheet()
+    format_sheet(WWT_length)
+    writer.sheets['WWT'].set_tab_color('blue')
 if show_IngramMX == True:
     df[IngramMX].to_excel(writer, sheet_name='IngramMX', index=False)
     worksheet = writer.sheets['IngramMX']
-    format_sheet()
-
-#color tabs
-writer.sheets['Main'].set_tab_color('yellow')
-writer.sheets['DSLC'].set_tab_color('green')
-writer.sheets['Roanoke'].set_tab_color('orange')
-writer.sheets['RLCA'].set_tab_color('red')
-writer.sheets['WWT'].set_tab_color('blue')
-writer.sheets['IngramMX'].set_tab_color('purple')
+    format_sheet(IngramMX_length)
+    writer.sheets['IngramMX'].set_tab_color('purple')
 
 writer.save()
